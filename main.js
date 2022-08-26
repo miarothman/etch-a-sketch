8345f41db75c7d04b054be849cc70414541dfb2e
miarothman <rothman.mia@gmail.com>
2022-08-26 13:45:27 -0400

    Add buttons and event listeners for the different drawing modes (classic, rainbow and eraser)

--- main.js
+++ main.js
@@ -9,21 +9,51 @@ for (let i = 0; i < 256; i++) {
 // create squares & adds them to the html grid container
 let square = document.createElement("div");
 // set style attributes to square divs
-square.setAttribute('style', 'background-color: ivory; border: 1px solid black; width: 40px; height: 40px;')
-//square.innerText = "Text";
+square.style.backgroundColor = 'white';
+//square.style.border = '1px solid #000';
+square.style.width = '40px';
+square.style.height = '40px';
 container.appendChild(square);
 }
 }
 
-// js event listener for hover effect example:
-// element.addEventListener("mouseover", myFunction);
-//element.addEventListener("mouseout", myThirdFunction);
-
+// changes the divs we hover over to GRAY
+// js event listener for mouseover hover effect
 let gridContainer = document.getElementById("container");
 gridContainer.addEventListener("mouseover", function( event ) {
-    event.target.style.backgroundColor = "gray";
-})
+event.target.style.backgroundColor = "gray";
+});
 
+
+// when user clicks COLOR MODE button, changes the divs we hover over to GRAY
+let grayBtn = document.getElementById("gray");
+grayBtn.addEventListener('click', function () {
+    gridContainer.addEventListener("mouseover", function ( grayEvent ) {
+        grayEvent.target.style.backgroundColor = "gray";
+    })
+});
+
+// random color function generator
+function randomColors () {
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+return "#" + randomColor;
+};
+
+// when user clicks RAINBOW MODE button, changes the divs we hover over to random colors using randomColors function
+let rainbowBtn = document.getElementById("rainbow");
+rainbowBtn.addEventListener('click', function () {
+    gridContainer.addEventListener("mouseover", function (rainbowEvent ) {
+        rainbowEvent.target.style.backgroundColor = randomColors();
+    })
+});
+
+// when user clicks ERASER MODE button, changes the divs we hover over to WHITE (eraser)
+let eraseBtn = document.getElementById("eraser");
+eraseBtn.addEventListener('click', function () {
+    gridContainer.addEventListener("mouseover", function( eraseEvent ) {
+    eraseEvent.target.style.backgroundColor = "#ffffff";
+})
+});
 
 // call createSquares function
 createSquares();
