70f55f12f2575dbcb68bc2fcf14bab6c6c11724e
miarothman <rothman.mia@gmail.com>
2022-08-26 00:12:06 -0400

    Make hover effect so on mouseover divs in canvas will have a filled in hover effect

--- main.js
+++ main.js
@@ -9,11 +9,21 @@ for (let i = 0; i < 256; i++) {
 // create squares & adds them to the html grid container
 let square = document.createElement("div");
 // set style attributes to square divs
-square.setAttribute('style', 'background-color: ivory; color: ivory; border: 1px solid black;')
-square.innerText = "Text";
+square.setAttribute('style', 'background-color: ivory; border: 1px solid black; width: 40px; height: 40px;')
+//square.innerText = "Text";
 container.appendChild(square);
 }
 }
+
+// js event listener for hover effect example:
+// element.addEventListener("mouseover", myFunction);
+//element.addEventListener("mouseout", myThirdFunction);
+
+let gridContainer = document.getElementById("container");
+gridContainer.addEventListener("mouseover", function( event ) {
+    event.target.style.backgroundColor = "gray";
+})
+
 
 // call createSquares function
 createSquares();
